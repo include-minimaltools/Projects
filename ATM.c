df91eabf4afe44d10953843b98f252178b37f817
Sawluis <luispineda200504@gmail.com>
2021-02-22 06:53:49 -1200

    TODo Heho

--- ATM.c
+++ ATM.c
@@ -1,6 +1,203 @@
-#include <stdio.h>
+#include <stdio.h>;
+#include <stdlib.h>;
+#include <conio.h>;
+#include <dos.h>;
+#include <string.h>;
+
+#define MAX_SRTLN 256
+#define enye 164
+#define up 72
+#define down 80
+#define enter 13
+
+typedef struct
+{
+    char user[MAX_SRTLN];
+    int password;
+}accounts;
+
+void Start(void);
+void Menu(void);
+void RemoveCursor(int x, int y);
+void Exit(void);
+void Exit_Option(void);
+void Presentation(void);
+void Recargas(void);
+void ATM(void);
+
+int RemainingAttemps = 3;
 
 void main()
 {
-    printf("Hola Buenas soy el dino de la mitchell");
+    Start();
 }
+
+void Start(void)
+{
+    accounts assigned, c1;
+    strcpy(assigned.user,"luis");
+    assigned.password = 1402;
+
+    clrscr();
+    
+    if (RemainingAttemps == 0) 
+    {
+        Exit();
+    }else{
+        gotoxy(10,6);
+        printf("Usuario : ");
+        cscanf("%s",&c1.user);
+
+        gotoxy(10,8);
+        printf("Contrase%ca : ",enye);
+        cscanf("%d",&c1.password);
+
+        if (strcmp(c1.user,assigned.user)==0 && c1.password == assigned.password)
+        {
+            Menu();
+        }else if (strcmp(c1.user,assigned.user)==0 && c1.password != assigned.password)
+        {
+            gotoxy(10,10);
+            printf("Contrase%ca Incorrecta",enye);
+            RemainingAttemps = RemainingAttemps - 1;
+            gotoxy(10,12);
+            printf("Le restan %d intentos",RemainingAttemps);
+            sleep(2);
+            Start();
+        }else if (strcmp(c1.user,assigned.user)!=0 && c1.password == assigned.password)
+        {
+            gotoxy(10,10);
+            printf("Usuario Incorrecto");
+            RemainingAttemps = RemainingAttemps - 1;
+            gotoxy(10,12);
+            printf("Le restan %d intentos",RemainingAttemps);
+            sleep(2);
+            Start();
+        }else if (strcmp(c1.user,assigned.user)!=0 && c1.password != assigned.password)
+        {
+            gotoxy(10,10);
+            printf("Usuario y Contrase%ca Incorrectos",enye);
+            RemainingAttemps = RemainingAttemps - 1;
+            gotoxy(10,12);
+            printf("Le restan %d intentos",RemainingAttemps);
+            sleep(2);
+            Start();
+        }
+        }
+}
+
+void Menu(void)
+{
+    int i,tecla,opc,option = 1,exit = 0;
+    char *menu_options[] = {"Presentacion","Cajero ATM-UNI","Recargas Tigo-Claro","Salir","\0"};
+
+    do
+    {
+        clrscr();
+        gotoxy(9,11 + option);
+        printf("-> ");
+
+        for(i = 0; i<4; i++)
+        {
+            gotoxy(12,12 + i);
+            printf("%s",menu_options[i]);
+        }
+
+        do
+        {
+            tecla = getch();
+        } while (tecla != enter && tecla != up && tecla != down);
+
+        switch (tecla)
+        {
+        case up:
+            option--;
+            if (option < 1)
+            {
+                option=4;
+            }
+            break;
+        case down:
+            option++;
+            if (option > 4)
+            {
+                option=1;
+            }
+            break;
+        case enter:
+            exit=1;
+        default:
+            break;
+        }
+    }while (exit == 0);
+
+    opc = option;
+
+    switch (opc)
+		{
+		case 1:
+			Presentation();
+			break;
+		case 2:
+			ATM();
+			break;
+		case 3:
+			Recargas();
+			break;
+		case 4:
+			Exit_Option();
+			break;
+		default:
+			break;
+		}
+}
+
+void Presentation(void)
+{
+    clrscr();
+    printf("Gabriel Gay");
+    getch();
+    Menu();
+}
+
+void ATM(void)
+{
+    clrscr();
+    printf("Hola numero 2");
+    getch();
+    Menu();
+}
+
+void Recargas(void)
+{
+    clrscr();
+    printf("Hola numero 3");
+    getch();
+    Menu();
+}
+
+void Exit_Option(void)
+{
+    clrscr();
+    printf("Hola numero 4");
+    getch();
+}
+
+void RemoveCursor(int x, int y)
+{
+    textcolor(BLACK);
+    gotoxy(x,y);
+    cprintf("%c",219);
+    gotoxy(x,y);
+}
+
+void Exit(void)
+{
+    clrscr();
+    printf("Ha agotado sus 3 intentos\n");
+    printf("-------------------------\n");
+    printf("Estimado Usuario : luis\n");
+    printf("Su cuenta sera bloqueada por 24 horas por motivos de seguridad\n");
+    printf("Gracias por su comprension");
+    getch();
+}
