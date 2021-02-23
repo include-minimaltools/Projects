2544259c3f44a870d9084a26e21e9ae82129f9c9
AlizSystems <gabrielortizamador@gmail.com>
2021-02-23 15:30:13 -0600

    Library

---
+++ Tools.h
@@ -0,0 +1,14 @@
+/* Herramientas para l trabajo de strings */
+
+#define MaxStrLen 5
+
+void getstring (char string[])
+{
+    int letter;
+    
+    while (letter < MaxStrLen)
+    {
+                
+
+    }
+}
