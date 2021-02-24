8427ed3192147174832efde322206f515b7c7bd6
AlizSystems <gabrielortizamador@gmail.com>
2021-02-24 12:44:42 -0600

    Tools.h v1.0

--- include/Tools.h
+++ include/Tools.h
@@ -1,3 +1,7 @@
+#ifndef stdbool.h
+#include "c:\\TC20\\repos\\projects\\include\\STDBOOL.h"
+#endif
+
 typedef char *string;
 
 enum allowed_characters
@@ -9,34 +13,33 @@ enum allowed_characters
     NumbersAndSymbols
 };
 
-int characters;
+int allowed_characters;
 bool isPassword;
 int MaxStrLn = 80;
 int MinStrLn = 2;
 int charPassword = 42;
 
-string get_string(char *message)
+string get_string(string message)
 {
-    string text;
     int letter = 0;
     char temp_string[80]={'\0'};
 
-    printf("%s", message);
+    cprintf("%s", message);
     while (letter < MaxStrLn)
     {
         temp_string[letter] = getch();
 
-        if ((characters == LettersAndSymbols || characters == NumbersAndSymbols || characters == Alls) && letter < MaxStrLn - 1 && ((temp_string[letter] >= 33 && temp_string[letter] <= 47) || (temp_string[letter] >= 58 && temp_string[letter] <= 64) || (temp_string[letter] >= 91 && temp_string[letter] <= 96) || (temp_string[letter] >= 123 && temp_string[letter] <= 126)))
+        if ((allowed_characters == LettersAndSymbols || allowed_characters == NumbersAndSymbols || allowed_characters == Alls) && letter < MaxStrLn - 1 && ((temp_string[letter] >= 33 && temp_string[letter] <= 47) || (temp_string[letter] >= 58 && temp_string[letter] <= 64) || (temp_string[letter] >= 91 && temp_string[letter] <= 96) || (temp_string[letter] >= 123 && temp_string[letter] <= 126)))
         {
             cprintf("%c", isPassword ? charPassword : temp_string[letter]);
             letter++;
         }
-        else if ((characters == OnlyNumbers || characters == Alls) && letter < MaxStrLn - 1 && temp_string[letter] >= 48 && temp_string[letter] <= 57)
+        else if ((allowed_characters == OnlyNumbers || allowed_characters == Alls) && letter < MaxStrLn - 1 && temp_string[letter] >= 48 && temp_string[letter] <= 57)
         {
             cprintf("%c", isPassword ? charPassword : temp_string[letter]);
             letter++;
         }
-        else if ((characters == OnlyLetters || characters == LettersAndSymbols || characters == Alls) && letter < MaxStrLn - 1 && ((temp_string[letter] >= 65 && temp_string[letter] <= 90) || (temp_string[letter] >= 97 && temp_string[letter] <= 122) || (temp_string[letter] >= 48 && temp_string[letter] <= 57)))
+        else if ((allowed_characters == OnlyLetters || allowed_characters == LettersAndSymbols || allowed_characters == Alls) && letter < MaxStrLn - 1 && ((temp_string[letter] >= 65 && temp_string[letter] <= 90) || (temp_string[letter] >= 97 && temp_string[letter] <= 122) || (temp_string[letter] >= 48 && temp_string[letter] <= 57)))
         {
             cprintf("%c", isPassword ? charPassword : temp_string[letter]);
             letter++;
@@ -58,6 +61,6 @@ string get_string(char *message)
             break;
         }
     }
-    memcpy(text, temp_string, sizeof(temp_string));
-    return text;
-}
+    strcpy(message, temp_string);
+    return message;
+}
