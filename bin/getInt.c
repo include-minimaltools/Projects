0b4fc932fd8feca9cf4d6b3c55fbd1f1ea6a49ef
Mitch-coder <michellescalderon2002@gmail.com>
2021-02-25 14:07:57 -0600

    Incorporacion de get_int

--- bin/getInt.c
+++ bin/getInt.c
@@ -1,55 +1,67 @@
 #include<conio.h>
 #include <stdio.h>
-/*todo que escriba el mensaje a mostrar y luego uq le permita ingresar su numero*/ 
+#include <string.h>
+#include <stdlib.h>
+int bool = 0;
 typedef char *string;
 
-int get_int(string message);
+int get_int(string message,int x,int y);
 
 int main()
 {
     int recive = 0;
     clrscr();
    
-    recive = get_int("Ingrese su numero: ");
-
-   /* ------Para confirmar la conversion de valor
-   printf("\nEste es el nuevo %d",recive);
+    recive = get_int("Ingrese su numero: ",1,1);
+    printf("\nEste es el nuevo %d",recive);
     getch();
-    */
+    
     return 0;    
 }
-int get_int(string message)
+int get_int(string message,int x, int y)
 {
-    int max = 6;
+    int max = 5;
     char assistant = ' ';
-    int i = 0; 
-    int intiger = 0;
+    int i = 0;
+    long int  intiger = 0;
     char hold_intiger[6];
-
-    cprintf("%s", message);
-    
-
     fflush(stdin);
     textcolor(WHITE);
+    gotoxy(x,y);
+    clreol();
+    cprintf("%s", message);
+
+    /*if (bool==0)
+    {
+       gotoxy(x,y);
+       clreol();
+       cprintf("%s", message);
+    }*/
+   
     /*---------------Ciclo infinito------------*/
-    //-32768 a 32767 Alcande del int 
-    // si es positivo el arreglo tendra dimension de cinco y si es negativo 6
-   //ya esta lo de las direccionales, la conversion , falta hacer que si es negativo no le deje poner un cero, 
+   
+    /*si es positivo el arreglo tendra dimension de cinco y si es negativo 6*/
+
     for (;;)
     {
         assistant = getch();
 
         if (assistant> 47 && assistant < 58 && i < max) 
         {
-            hold_intiger[i] = assistant;
-            cprintf("%c", hold_intiger[i]);
-            i++;
+            if (assistant==48 && hold_intiger[0]==45 && i == 1 ){}
+            else
+            {
+              hold_intiger[i] = assistant;
+              cprintf("%c", hold_intiger[i]);
+              i++;
+            }
         }
         /*Aqui solo se imprimira el negativo si no hay un numero antes*/
         else if (assistant==45 && i==0) 
         {
             hold_intiger[i] = assistant;
             cprintf("%c", hold_intiger[i]);
+            max = 6;
             i++;
         }
         else if (assistant == 8 && i > 0)
@@ -68,8 +80,20 @@ int get_int(string message)
             hold_intiger[i]= '\0';
             break;
         }
+        if (i==0)
+        {
+            max = 5;
+        }
 
     }
-    intiger = atoi(hold_intiger);
-    return intiger; 
+    /*Fuera del ciclo preguntamos*/
+    intiger = atol(hold_intiger);
+     /*-32768 a 32767 Alcande del int */
+    if( intiger > -32768 && intiger < 32767)
+       return intiger;
+    else
+    {
+      bool = 1;
+      return (get_int(message,x,y)); 
+    }
 }   
